7f44cccc174a5d2e10b03e95f7423379ec2e47cd
dylan <dylan@dylanwolf.com>
2017-12-23 20:12:59 -0500

    Updated for WebGL build

---
+++ Assets/Scripts/AndroidExit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidExit : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+			Application.Quit();
+	}
+}
